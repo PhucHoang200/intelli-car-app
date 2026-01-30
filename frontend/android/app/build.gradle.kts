3332d430a5f2d84f95566cf2a0a9edefa031a509
PhucHoang200 <phucphuc2004444@gmail.com>
2026-01-30 20:31:04 +0700

    add feature logout

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
 //        minSdk = flutter.minSdkVersion
 //        targetSdk = 35
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
