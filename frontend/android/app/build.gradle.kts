23caf87f1ae2fe082e106eb93f5207edc7209412
PhucHoang200 <phucphuc2004444@gmail.com>
2026-02-01 13:06:30 +0700

    solve more changes

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
 //        minSdk = flutter.minSdkVersion
 //        targetSdk = 35
-//      minSdk = 23
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
+//        minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
